1acd0d8e3c230d4c766e54dfe8ecccd19c024e2a
yechan <kevin2695@naver.com>
2024-04-25 21:50:51 +0900

    [240425] 패키지 추가

---
+++ packages/common/index.d.ts
@@ -0,0 +1,1 @@
+import './hooks.d.ts';
--- packages/common/vite.config.ts
+++ packages/common/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: {
+        hooks: './src/hooks/index.ts',
+      },
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
 });
